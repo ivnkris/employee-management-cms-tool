6fd8d07d8780900906d238a5c32ea70a222c6d4b
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-05-23 12:39:04 +0100

    added utility methods for DB queries and parametised queries

--- src/db/DB.js
+++ src/db/DB.js
@@ -34,6 +34,38 @@ class DB {
       message || `Connection to ${this.database} has been successfully closed.`
     );
   }
+
+  query(sqlQuery) {
+    return new Promise((resolve, reject) => {
+      const handleQuery = (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(rows);
+      };
+
+      this.connection.query(sqlQuery, handleQuery);
+    });
+  }
+
+  parameterisedQuery(sqlQuery, args, info = false) {
+    return new Promise((resolve, reject) => {
+      const handleQuery = (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(rows);
+      };
+
+      const query = this.connection.query(sqlQuery, args, handleQuery);
+
+      if (info) {
+        console.log(query.sql);
+      }
+    });
+  }
 }
 
 module.exports = DB;
