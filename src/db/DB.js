d8bf086ec23c62bfbf15e833a06b7458e29d01bd
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-05-23 12:23:30 +0100

    added DB constructor

--- src/db/DB.js
+++ src/db/DB.js
@@ -1,7 +1,18 @@
 const mysql = require("mysql");
 
 class DB {
-  constructor() {}
+  constructor(database) {
+    const databaseOptions = {
+      host: "localhost",
+      user: "root",
+      password: "password",
+      port: 3306,
+      database,
+    };
+
+    this.database = database;
+    this.connection = mysql.createConnection(databaseOptions);
+  }
 }
 
 module.exports = DB;
