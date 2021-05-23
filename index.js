92342f17d0c1337c8e610dadae524557dfd320d7
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-05-23 13:59:58 +0100

    added function to view all employees

--- index.js
+++ index.js
@@ -28,6 +28,19 @@ const viewRoles = async () => {
   console.log("Press the Up or Down arrow to return to the menu options");
 };
 
+const viewEmployees = async () => {
+  const db = new DB("employee_management_system");
+
+  const allEmployees = await db.query(
+    'SELECT a.id as "Employee ID", a.first_name as "First Name", a.last_name as "Last Name", title as "Title", CONCAT(b.first_name," ",b.last_name) as "Manager\'s Name", name as "Department" FROM employee a LEFT JOIN employee b on a.manager_id = b.id LEFT JOIN role ON a.role_id = role.id LEFT JOIN department ON role.department_id = department.id'
+  );
+  console.log("\n");
+  console.table(allEmployees);
+  console.log("\n");
+  console.log("\n");
+  console.log("Press the Up or Down arrow to return to the menu options");
+};
+
 const mainMenu = async () => {
   const menuQuestions = [
     {
@@ -57,7 +70,7 @@ const mainMenu = async () => {
     } else if (menuOption.menuChoices === "View all roles") {
       viewRoles();
     } else if (menuOption.menuChoices === "View all employees") {
-      console.log("viewing employees");
+      viewEmployees();
     } else if (menuOption.menuChoices === "Add new department") {
       console.log("adding new department");
     } else if (menuOption.menuChoices === "Add new role") {
